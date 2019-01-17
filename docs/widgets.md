0bb469f3105fcce48721b86ae6cda83525f5370b
Aravindh Sridharan <mail@aravindh.io>
2019-01-17 18:20:15 +0100

    added search result item

---
+++ docs/widgets.md
@@ -0,0 +1,7 @@
+* Quick actions like create table
+
+* Search Widget
+* Table widget
+    * Projection settings
+* Table to display array of objects
+* Reloadable widget
--- src/components/Widgets/SearchWidget.js
+++ src/components/Widgets/SearchWidget.js
@@ -2,10 +2,34 @@ import React from 'react';
 import { Input } from 'antd';
 
 const Search = Input.Search;
-export const SearchWidget = (props) => (
-    <Search
-        placeholder="input search text"
-        onSearch={value => props.action({ id: value, ...props })}
-        style={{ width: 200 }}
-    />
+export class SearchWidget extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasResult: false, result: null }
+    }
+    onSearch(props) {
+        return value => {
+            const searchResult = props.action({ id: value, ...props });
+            this.setState({ hasResult: true, result: searchResult });
+        }
+    }
+    render() {
+        return !this.state.hasResult ? (
+            <Search
+                onSearch={this.onSearch(this.props)}
+                style={{ width: 200 }}
+            />
+        ) : (
+                <div>
+                    <Search
+                        onSearch={this.onSearch(this.props)}
+                        style={{ width: 200 }}
+                    />
+                    {SearchResultDisplay(this.state.result)}
+                </div>
+            )
+    }
+}
+export const SearchResultDisplay = (result) => (
+    <div>{JSON.stringify(result)}</div>
 )
